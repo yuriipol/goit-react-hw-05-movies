bdfc336da810118bbca414bbcce57517b4a73b61
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-08-29 11:23:25 +0000

    Deploying to gh-pages from @ yuriipol/goit-react-hw-05-movies@f615b53158ab1e7db7d8b1d095856aea8472e863 🚀

---
+++ static/js/961.adfc95a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[961],{2961:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r={},a=n(184),c=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{className:r.title,children:"Page Not Found!"})})}}}]);
+//# sourceMappingURL=961.adfc95a4.chunk.js.map
